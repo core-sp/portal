c9e3528df6545075ae4eb77a2883152f766e7223
arbex10 <lucasarbexmb@gmail.com>
2019-04-05 07:56:30 -0300

    Sidebar Content

--- resources/views/site/pagina.blade.php
+++ resources/views/site/pagina.blade.php
@@ -3,22 +3,29 @@
 @section('content')
 
 <section id="pagina-cabecalho" class="mt-1">
-	<div class="container-fluid text-center nopadding position-relative pagina-titulo-img">
-		<img src="{{asset($pagina->img)}}" />
-		<div class="row position-absolute pagina-titulo">
-			<div class="container text-center">
-				<h1 class="branco text-uppercase">
-					{{ $pagina->titulo }}
-				</h1>
-			</div>
-		</div>
-	</div>
+  <div class="container-fluid text-center nopadding position-relative pagina-titulo-img">
+    <img src="{{asset($pagina->img)}}" />
+    <div class="row position-absolute pagina-titulo">
+      <div class="container text-center">
+        <h1 class="branco text-uppercase">
+          {{ $pagina->titulo }}
+        </h1>
+      </div>
+    </div>
+  </div>
 </section>
 
 <section id="pagina-conteudo">
-	<div class="container conteudo-txt">
-		{!! $pagina->conteudo !!}
+  <div class="container">
+    <div class="row">
+	  <div class="col-sm-8 conteudo-txt pr-4">
+	  {!! $pagina->conteudo !!}
+	  </div>
+	  <div class="col-sm-4">
+	  	@include('site.inc.content-sidebar')
+	  </div>
 	</div>
+  </div>
 </section>
 
 @endsection
