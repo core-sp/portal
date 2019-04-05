9e1976381ec1c8dc28dce66aa1242d65dc6ea040
arbex10 <lucasarbexmb@gmail.com>
2019-04-05 07:56:30 -0300

    Sidebar Content

---
+++ resources/views/site/inc/content-sidebar.blade.php
@@ -0,0 +1,60 @@
+<a href="https://api.whatsapp.com/send?phone=551132435516&text=Olá%20CORE-SP!" target="_blank">
+  <div class="box-dois zap-bg mb-3">
+    <div class="inside-box-dois d-flex">
+      <div class="align-self-center">
+        <img src="http://127.0.0.1:8000/img/zap.png" class="inside-img">
+      </div>
+      <div class="flex-one align-self-center pl-4">
+        <h5 class="text-uppercase normal branco">Acompanhe as últimas notícias</h5>
+      </div>
+    </div>
+  </div>
+</a>
+<a href="https://youtube.com" target="_blank">
+  <div class="box-dois vermelho-bg mb-3">
+    <div class="inside-box-dois d-flex">
+      <div class="align-self-center">
+        <img src="http://127.0.0.1:8000/img/youtube.png" class="inside-img">
+      </div>
+      <div class="flex-one align-self-center pl-4">
+        <h5 class="text-uppercase normal branco">Confira nosso canal no YouTube</h5>
+      </div>
+    </div>
+  </div>
+</a>
+<a href="/balcao-de-oportunidades">
+  <div class="box-dois azul-bg mb-3">
+    <div class="inside-box-dois d-flex">
+      <div class="align-self-center">
+        <img src="http://127.0.0.1:8000/img/001-work.png" class="inside-img">
+      </div>
+      <div class="flex-one align-self-center pl-4">
+        <h5 class="text-uppercase normal branco">Balcão de Oportunidades</h5>
+      </div>
+    </div>
+  </div>
+</a>
+<a href="/cursos">
+  <div class="box-dois azul-escuro-bg mb-3">
+    <div class="inside-box-dois d-flex">
+      <div class="align-self-center">
+        <img src="http://127.0.0.1:8000/img/teacher.png" class="inside-img">
+      </div>
+      <div class="flex-one align-self-center pl-4">
+        <h5 class="text-uppercase normal branco">Conheça nossos cursos</h5>
+      </div>
+    </div>
+  </div>
+</a>
+<a href="#">
+  <div class="box-dois azul-bg mb-3">
+    <div class="inside-box-dois d-flex">
+      <div class="align-self-center">
+        <img src="http://127.0.0.1:8000/img/003-bill.png" class="inside-img">
+      </div>
+      <div class="flex-one align-self-center pl-4">
+        <h5 class="text-uppercase normal branco">Emissão de Boleto Anuidade 2019</h5>
+      </div>
+    </div>
+  </div>
+</a>
