c1e90e757688b66e3de3de945e88d6e03229b770
arbex10 <lucasarbexmb@gmail.com>
2019-05-10 17:34:20 -0300

    Teste de Deploy

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -22,7 +22,7 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-sm-12">
-      	<h1>Home / Deploy Automatizado</h1>
+      	<h1>Home</h1>
       </div>
     </div>
   </div>
