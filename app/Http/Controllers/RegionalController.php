2e1e5e89c51a7c25e8f5baf1e5893ba41c55a38e
arbex10 <lucasarbexmb@gmail.com>
2019-05-13 11:01:20 -0300

    Redis Regionais

--- app/Http/Controllers/RegionalController.php
+++ app/Http/Controllers/RegionalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use App\Regional;
 use App\Noticia;
 use App\Http\Controllers\ControleController;
--- app/Http/Controllers/RegionalSiteController.php
+++ app/Http/Controllers/RegionalSiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use App\Regional;
 use App\Noticia;
 
