226de6ff4b5a119278547f1611cb427edeef5b69
arbex10 <lucasarbexmb@gmail.com>
2019-05-14 14:00:37 -0300

    Otimizações

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -15,13 +15,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     public function boot()
     {
-        parent::boot();
-
-        // Horizon::routeSmsNotificationsTo('15556667777');
-        // Horizon::routeMailNotificationsTo('example@example.com');
-        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
-        
-        // Horizon::night();
+        $this->middleware('auth');
     }
 
     /**
