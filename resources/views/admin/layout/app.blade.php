addd1ec056a30666b726a9d7fe74664a2e059c71
arbex10 <lucasarbexmb@gmail.com>
2019-06-14 08:50:00 -0300

    Pequenas alterações

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -95,7 +95,7 @@
 
           <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-              <b>Versão</b> 1.0.10
+              <b>Versão</b> 1.0.11
             </div>
             <strong>Portal CORE-SP &copy; 2019.</strong> Todos direitos reservados.
           </footer>
