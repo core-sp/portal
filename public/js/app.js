8edfaebb7acdca0efbb8f0ffc30adfe542951490
arbex10 <lucasarbexmb@gmail.com>
2019-06-18 10:16:29 -0300

    Corrige bug de data inválida agendamento

--- public/js/app.js
+++ public/js/app.js
@@ -36942,7 +36942,7 @@ window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
  */
 
 try {
-  window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js").default;
+  window.Popper = __webpack_require__(/*! popper.js */ "./node_modules/popper.js/dist/esm/popper.js")["default"];
   window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
   __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
