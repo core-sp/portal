f9ed4554d83fd2db91d74759e129e6d3aa0a4acf
arbex10 <lucasarbexmb@gmail.com>
2019-06-28 16:38:28 -0300

    Mudanças na home page do Admin v4

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,11 +3,8 @@
 @section('content')
 
 @php
-	use App\Http\Controllers\NewsletterController;
 	use App\Http\Controllers\ControleController;
 	use App\Http\Controllers\Helper;
-	$totalAgendamentos = App\Http\Controllers\Helpers\AgendamentoControllerHelper::countAgendamentos();
-	$totalInscritos = App\Http\Controllers\Helpers\CursoHelper::totalInscritos();
 	$alertas = App\Http\Controllers\AdminController::alertas();
 	$chamados = App\Http\Controllers\Helpers\ChamadoControllerHelper::getByUser(Auth::user()->idusuario);
 	$count = App\Http\Controllers\AdminController::countAtendimentos();
@@ -51,50 +48,7 @@
       </div>
     </div>
 	<div class="row mt-3">
-	  <div class="col">
-		<div class="info-box">
-		  <span class="info-box-icon bg-success">
-		  	<i class="fas fa-globe"></i>
-		  </span>
-		  <div class="info-box-content">
-			<span class="info-box-text">Visitas no último mês</span>
-			<span class="info-box-number">40.229</span>
-		  </div>
-		</div>
-	  </div>
-	  <div class="col">
-		<div class="info-box">
-		  <span class="info-box-icon bg-danger">
-		  	<i class="fas fa-user-clock"></i>
-		  </span>
-		  <div class="info-box-content">
-			<span class="info-box-text">Agendamentos</span>
-			<span class="info-box-number">{{ $totalAgendamentos }}</span>
-		  </div>
-		</div>
-	  </div>
-	  <div class="col">
-		<div class="info-box">
-		  <span class="info-box-icon bg-warning">
-		  	<i class="fas fa-chalkboard-teacher text-white"></i>
-		  </span>
-		  <div class="info-box-content">
-			<span class="info-box-text">Inscritos em cursos</span>
-			<span class="info-box-number">{{ $totalInscritos }}</span>
-		  </div>
-		</div>
-	  </div>
-	  <div class="col">
-		<div class="info-box">
-		  <span class="info-box-icon bg-info">
-			<i class="fas fa-newspaper"></i>
-		  </span>
-		  <div class="info-box-content">
-			<span class="info-box-text">Inscritos na Newsletter</span>
-			<span class="info-box-number">{{ NewsletterController::countNewsletter() }}</span>
-		  </div>
-		</div>
-	  </div>
+		@include('admin.inc.clipshome')
 	</div>
   </div>
 </section>
