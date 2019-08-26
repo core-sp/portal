3879159d47b33e2e7a5a84f4ee247c60c98fefc4
arbex10 <lucasarbexmb@gmail.com>
2019-08-26 09:49:28 -0300

    Correção bug nos cursos

--- app/Http/Controllers/CursoSiteController.php
+++ app/Http/Controllers/CursoSiteController.php
@@ -13,7 +13,7 @@ class CursoSiteController extends Controller
     public function cursosView()
     {
         $now = date('Y-m-d H:i:s');
-        $cursos = Curso::select('idcurso','img','idregional','tipo','tema','resumo')
+        $cursos = Curso::select('idcurso','img','idregional','tipo','tema','resumo', 'datarealizacao')
                        ->where('datatermino','>=',$now)
                        ->where('publicado','Sim')
                        ->paginate(10);
