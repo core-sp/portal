c09689bf9ff741798ed1f3c00a0b507a5d4d0dd2
arbex10 <lucasarbexmb@gmail.com>
2019-10-04 15:09:05 -0300

    Anúncio de vagas (final)

--- app/Http/Controllers/BdoSiteController.php
+++ app/Http/Controllers/BdoSiteController.php
@@ -167,7 +167,7 @@ class BdoSiteController extends Controller
 
         $this->saveBdoOportunidade($this->idempresa);
 
-        Mail::to('desenvolvimento@core-sp.org.br')->queue(new AnunciarVagaMail($this->bodyEmail($this->idoportunidade)));
+        Mail::to('informacoes@core-sp.org.br')->queue(new AnunciarVagaMail($this->bodyEmail($this->idoportunidade)));
 
         return view('site.agradecimento')->with([
             'agradece' => $this->agradecimento()
