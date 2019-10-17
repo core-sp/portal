2eedee251deb527b56a5376b1387364b44cb29b9
arbex10 <lucasarbexmb@gmail.com>
2019-10-17 10:40:42 -0300

    Migration para Representantes

---
+++ database/migrations/2019_10_17_103314_create_representantes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepresentantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('representantes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('cpf_cnpj')->unique();
+            $table->string('registro_core')->nullable();
+            $table->string('email');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('representantes');
+    }
+}
