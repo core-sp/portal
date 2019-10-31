c5118b9bca00975cc2c344c137a38337ce55f6e8
arbex10 <lucasarbexmb@gmail.com>
2019-10-31 15:20:02 -0300

    Fiscalização e Novembro Azul

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -155,7 +155,7 @@
       <div class="col-12">
         <div class="text-center novo-core-box">
           <h2 class="stronger branco text-uppercase">um novo core-sp para você!</h2>
-          <a href="/arquivos/2019-07/Informativo%20Core-SP_agosto-compactado.pdf" class="btn-novo-core" download><h4 class="normal">Informativo</h4></a>
+          <a href="/noticia/informativo-core-sp" class="btn-novo-core"><h4 class="normal">Informativo</h4></a>
           <br class="show-576 br-novo-core" />
           <a href="/acoes-da-fiscalizacao" class="btn-novo-core"><h4 class="normal">Fiscalização</h4></a>
           <br class="show-576 br-novo-core" />
