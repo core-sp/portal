6873cdc525feb8ff30f89d6c5cdf4ffe4db5d3cf
arbex10 <lucasarbexmb@gmail.com>
2020-01-03 11:09:24 -0300

    Add tipos de serviço agendamento

--- resources/views/site/inc/content-sidebar.blade.php
+++ resources/views/site/inc/content-sidebar.blade.php
@@ -11,6 +11,18 @@
       </div>
     </div>
   </a>
+  <a href="/agendamento">
+    <div class="box-dois azul-escuro-bg mb-3">
+      <div class="inside-box-dois d-flex">
+        <div class="align-self-center">
+          <img src="{{ asset('img/appointment.png') }}" class="inside-img">
+        </div>
+        <div class="flex-one align-self-center pl-4">
+          <h5 class="text-uppercase normal branco">Agendamento de atendimento</h5>
+        </div>
+      </div>
+    </div>
+  </a>
   <a href="/representante/login">
     <div class="box-dois azul-bg mb-3">
       <div class="inside-box-dois d-flex">
