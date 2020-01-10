a7844d8aaba875ca6240fae920d3b8e0dd804be7
arbex10 <lucasarbexmb@gmail.com>
2020-01-10 13:34:17 -0300

    Add status em secondLine method

--- app/helpers.php
+++ app/helpers.php
@@ -352,6 +352,8 @@ function secondLine($situacao, $vencimento = null, $link = null, $descricao = nu
         $str = '<strong class="text-warning">EM ABERTO</strong> ⋅ <a href="https://boletoonline.caixa.gov.br/ecobranca/SIGCB/imprimir/0779951/' . $boleto . '" class="normal text-info" onclick="clickBoleto(\''. $descricao .'\')">BAIXAR BOLETO</a>';
     } elseif($situacao === 'Pago') {
         $str = '<strong class="text-success">PAGO</strong>';
+    } elseif($situacao = 'Pago em Parcelamento') {
+        $str = '<strong class="text-success">PAGO EM PARCELAMENTO</strong>';
     } elseif($situacao = 'Proc. Adm.') {
         $str = '<strong class="text-info">PROC. ADM.</strong>';
     } else {
