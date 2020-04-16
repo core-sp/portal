19ed17db83219adfbdcc87bc0574146b36440a41
arbexmb <lucasarbexmb@gmail.com>
2020-04-16 16:00:22 -0300

    Add imagem banner principal

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,12 +7,14 @@ $(document).ready(function(){
   $('#lfm-4').filemanager('image');
   $('#lfm-5').filemanager('image');
   $('#lfm-6').filemanager('image');
+  $('#lfm-7').filemanager('image');
   $('#lfm-m-1').filemanager('image');
   $('#lfm-m-2').filemanager('image');
   $('#lfm-m-3').filemanager('image');
   $('#lfm-m-4').filemanager('image');
   $('#lfm-m-5').filemanager('image');
   $('#lfm-m-6').filemanager('image');
+  $('#lfm-m-7').filemanager('image');
   $('#edital').filemanager('file');
   // Máscaras gerais
   $('.nrlicitacaoInput').mask('999/9999');
