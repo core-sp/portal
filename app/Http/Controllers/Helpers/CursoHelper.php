aeff12ca8f42b96d79d35a949a450c04ddbedaa9
arbexmb <lucasarbexmb@gmail.com>
2020-05-08 15:52:14 -0300

    Add Live ao tipo de Curso

--- app/Http/Controllers/Helpers/CursoHelper.php
+++ app/Http/Controllers/Helpers/CursoHelper.php
@@ -14,6 +14,7 @@ class CursoHelper extends Controller
         $tipos = [
             'Curso',
             'Evento Comemorativo',
+            'Live',
             'Palestra',
             'Workshop'
         ];
