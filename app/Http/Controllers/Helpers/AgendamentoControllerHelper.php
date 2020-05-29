dde67590a299c240c35295ff17d750ede38320cd
arbexmb <lucasarbexmb@gmail.com>
2020-05-29 18:12:29 -0300

    Volta agendamento.

--- app/Http/Controllers/Helpers/AgendamentoControllerHelper.php
+++ app/Http/Controllers/Helpers/AgendamentoControllerHelper.php
@@ -26,22 +26,12 @@ class AgendamentoControllerHelper extends Controller
     {
         $horas = [
             '09:00',
-            '09:30',
             '10:00',
-            '10:30',
             '11:00',
-            '11:30',
             '12:00',
-            '12:30',
             '13:00',
-            '13:30',
             '14:00',
-            '14:30',
-            '15:00',
-            '15:30',
-            '16:00',
-            '16:30',
-            '17:00',
+            '15:00'
         ];
         $bloqueios = AgendamentoBloqueio::where('idregional',$regional)
             ->whereDate('diainicio','<=',$dia)
@@ -77,22 +67,12 @@ class AgendamentoControllerHelper extends Controller
     {
         $horas = [
             '09:00',
-            '09:30',
             '10:00',
-            '10:30',
             '11:00',
-            '11:30',
             '12:00',
-            '12:30',
             '13:00',
-            '13:30',
             '14:00',
-            '14:30',
-            '15:00',
-            '15:30',
-            '16:00',
-            '16:30',
-            '17:00',
+            '15:00'
         ];
         return $horas;
     }
