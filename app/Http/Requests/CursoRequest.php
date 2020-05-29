d90e059b4ee6f0d151f4d957a8c611cb89e5d80c
arbexmb <lucasarbexmb@gmail.com>
2020-05-29 13:52:57 -0300

    Curso Request.

--- app/Http/Requests/CursoRequest.php
+++ app/Http/Requests/CursoRequest.php
@@ -22,7 +22,7 @@ class CursoRequest extends FormRequest
             'img' => 'max:255',
             'datarealizacao' => 'required',
             'datatermino' => 'required',
-            'endereco' => 'required|max:255',
+            'endereco' => 'required_unless:tema,Live|max:255',
             'nrvagas' => 'required|numeric',
             'descricao' => 'required'
         ];
