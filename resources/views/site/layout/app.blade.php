31c3c9ed73adb08f3906d0bfb55d6f2ec4df4b13
mikioi <mikioi@users.noreply.github.com>
2020-09-21 13:08:21 -0300

    Trocando a língua do ReCAPTCHA para português (Brasil).

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -368,7 +368,7 @@ use \App\Http\Controllers\Helper;
       <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
       <script type="text/javascript" src="{{ asset('/js/jquery.mask.js') }}"></script>
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.js"></script>
-      <script type="text/javascript" src='https://www.google.com/recaptcha/api.js'></script>
+      <script type="text/javascript" src='https://www.google.com/recaptcha/api.js?hl=pt-BR'></script>
       <script type="text/javascript" src="{{ asset('/js/site.js?'.time()) }}"></script>
       <script type="text/javascript" src="{{ asset('/js/acessibilidade.js') }}"></script>
     </body>
