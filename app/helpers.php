2d691ef242fafd935261336adc7c9e1352897e8b
mikioi <mikioi@users.noreply.github.com>
2020-11-06 13:47:23 -0300

    Modificando o horário em que o chat estará disponível no Portal.

--- app/helpers.php
+++ app/helpers.php
@@ -620,7 +620,7 @@ function mostraChatScript()
     if(config('app.env') !== 'local') {
         $hour = date('H');
         $day = date('w');
-        if($hour >= 10 && $hour < 16 && $day !== '6' && $day !== '0') {
+        if($hour >= 9 && $hour < 18 && $day !== '6' && $day !== '0') {
             return '<script src="//code.jivosite.com/widget/X12I8gg4Qy" async></script>';
         }
     }
