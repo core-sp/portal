2f25d319dd2020c7dda0a86561ee796b46cb00fb
mikioi <mikioi@users.noreply.github.com>
2021-05-27 18:13:42 -0300

    Melhorando interface.

--- resources/views/site/representante/calculadora-refis.blade.php
+++ resources/views/site/representante/calculadora-refis.blade.php
@@ -4,10 +4,33 @@
 
 <div class="representante-content w-100">
     <div class="nomargin conteudo-txt-mini light">
-        <h4 class="pt-1 pb-1">Calculadora Refis</h4>
+        <h4 class="pt-1 pb-1">Calculadora Refis para anuidades em aberto</h4>
         <div class="linha-lg-mini mb-3"></div>
         @if (!empty($anuidades))
-        <p>Selecione as anuidades em aberto para consultar valores com descontos.</p>
+        <p>Selecione as anuidades em aberto para consultar valores com descontos. Atenção, parcela deve ter o valor mínimo de R$ 100,00 (cem reais)</p>
+
+        <h5 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;Valores</h5>
+        <table class="table table-bordered bg-white mb-0">
+            <tbody>
+                <tr>
+                    <td class="ls-meio-neg">Total s/ desconto</td>
+                    <td class="ls-meio-neg"><div id="total" value="0">R$ 0,00</div></td>
+                </tr>
+                <tr>
+                    <td class="ls-meio-neg">Total c/ 90% de desconto<p class="text-left"><small>* pagamento à vista, no boleto ou em até 12 (doze) parcelas no cartão de crédito</small></p></td>
+                    <td class="ls-meio-neg"><div id="total90" value="0">R$ 0,00</div></td>
+                </tr>
+                <tr>
+                    <td class="ls-meio-neg">Total c/ 80% de desconto<p class="text-left"><small>* pagamento de 2 a 6 parcelas no boleto</small></p></td>
+                    <td class="ls-meio-neg"><div id="total80" value="0">R$ 0,00</div></td>
+                </tr>
+                <tr>
+                    <td class="ls-meio-neg">Total c/ 60% de desconto<p class="text-left"><small>* para pagamento de 7 a 12 parcelas no boleto</small></p></td>
+                    <td class="ls-meio-neg"><div id="total60" value="0">R$ 0,00</div></td>
+                </tr>
+            </tbody>
+        </table>
+
         <h5 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;Anuidades em aberto</h5>
         <table class="table table-bordered bg-white mb-0">
             <thead>
@@ -36,17 +59,9 @@
             </tbody>
         </table>
 
-        <div class="contatos-table space-single">
-            <h3 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;Total s/ desconto: <div id="total" value="0">R$ 0,00</div></h3>
-            <h3 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;Total c/ 90% de desconto: <div id="total90" value="0">R$ 0,00</div></h3>
-            <h3 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;Total c/ 80% de desconto: <div id="total80" value="0">R$ 0,00</div></h3>
-            <h3 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;Total c/ 60% de desconto: <div id="total60" value="0">R$ 0,00</div></h3>
-        </div>
-
         @else
         <p>Não foi encontrado nenhuma anuidade em aberto elegível para Refis. Não é possível consultar valores com descontos.</p>
         @endif
-
 
     </div>            
 </div>
