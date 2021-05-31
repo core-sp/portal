01622b377ab35bb8b4594833672c8f5afe01ade1
mikioi <mikioi@users.noreply.github.com>
2021-05-31 18:12:52 -0300

    Adicionando simulador no Admin.

--- resources/views/admin/views/representante.blade.php
+++ resources/views/admin/views/representante.blade.php
@@ -28,6 +28,9 @@
                         <a class="nav-link" id="vert-tabs-certidoes-tab" data-toggle="pill" href="#vert-tabs-certidoes" role="tab" aria-controls="vert-tabs-certidoes" aria-selected="false">
                             Certidões
                         </a>
+                        <a class="nav-link" id="vert-tabs-refis-tab" data-toggle="pill" href="#vert-tabs-refis" role="tab" aria-controls="vert-tabs-refis" aria-selected="false">
+                            Simulador Refis
+                        </a>
                     </div>
                 </div>
                 <div class="col-7 col-sm-9 setecinco">
@@ -179,8 +182,6 @@
                             @endif
                         </div>
 
-
-
                         <div class="tab-pane fade" id="vert-tabs-certidoes" role="tabpanel" aria-labelledby="vert-tabs-certidoes-tab">
                             @if (isset($certidoes))
                                 <h5 class="mb-2">
@@ -235,6 +236,92 @@
                             @else
                                 -----
                             @endif
+                        </div>
+
+                        <div class="tab-pane fade" id="vert-tabs-refis" role="tabpanel" aria-labelledby="vert-tabs-refis-tab">
+                        @if ($refis['total'] !== 0)
+                            <p>Abaixo valores com descontos disponíveis com seus respectivos parcelamentos. Atenção, parcela deve ter o valor mínimo de R$ 100,00 (cem reais).</p>
+
+                            <h5 class="mt-0 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;VALORES</h5>
+                            <table class="table table-bordered bg-white mb-0">
+                                <tbody>
+                                    <tr>
+                                        <td class="ls-meio-neg">Total s/ desconto</td>
+                                        <td class="ls-meio-neg"><div id="total" value="{{ $total }}">R$ {{ toReais($refis['total']) }}</div></td>
+                                    </tr>
+                                    <tr>
+                                        @if($refis['nParcelas90'][0] !== 0)
+                                            <td class="ls-meio-neg">Total c/ 90% de desconto<p class="text-left"><small>* pagamento à vista, no boleto ou em até {{ end($nParcelas90) }} parcelas no cartão de crédito</small></p></td>
+                                            <td class="ls-meio-neg"><div id="total90" value="{{ $total90 }}">R$ {{ toReais($refis['total90']) }}</div></td>
+                                            <td class="ls-meio-neg">
+                                                <select id="90" class="form-control nParcela">
+                                                    @foreach($refis['nParcelas90'] as $n)
+                                                        <option value="{{ $n }}">{{ $n }}x</option>
+                                                    @endforeach
+                                            </td>
+                                            <td id="parcelamento90" class="ls-meio-neg">R$ {{ toReais($refis['total90']/$refis['nParcelas90'][0]) }}</td>
+                                        @else
+                                            <td class="ls-meio-neg">Total c/ 90% de desconto<p class="text-left vermelho"><small>* não disponível devido ao valor mínimo de parcela</small></p></td>
+                                            <td class="ls-meio-neg"><div id="total90" value="0">-</div></td>
+                                            <td class="ls-meio-neg">-</td>
+                                            <td class="ls-meio-neg">-</td>
+                                        @endif
+                                    </tr>
+                                    <tr>
+                                        @if($refis['nParcelas80'][0] !== 0)
+                                            <td class="ls-meio-neg">Total c/ 80% de desconto<p class="text-left"><small>* pagamento de {{ $refis['nParcelas80'][0] }} a {{ end($refis['nParcelas80']) }} parcelas no boleto</small></p></td>
+                                            <td class="ls-meio-neg"><div id="total80" value="{{ $total80 }}">R$ {{ toReais($refis['total80']) }}</div></td>
+                                            <td class="ls-meio-neg">
+                                                <select id="80" class="form-control nParcela">
+                                                    @foreach($refis['nParcelas80'] as $n)
+                                                        <option value="{{ $n }}">{{ $n }}x</option>
+                                                    @endforeach
+                                            </td>
+                                            <td id="parcelamento80" class="ls-meio-neg">R$ {{ toReais($refis['total80']/$refis['nParcelas80'][0]) }}</td>
+                                        @else
+                                            <td class="ls-meio-neg">Total c/ 80% de desconto<p class="text-left vermelho"><small>* não disponível devido ao valor mínimo de parcela</small></p></td>
+                                            <td class="ls-meio-neg"><div id="total80" value="0">-</div></td>
+                                            <td class="ls-meio-neg">-</td>
+                                            <td class="ls-meio-neg">-</td>
+                                        @endif
+                                    </tr>
+                                    <tr>
+                                        @if($refis['nParcelas60'][0] !== 0)
+                                            <td class="ls-meio-neg">Total c/ 60% de desconto<p class="text-left"><small>* pagamento de {{ ($refis['nParcelas60'][0] }} a {{ end($refis['nParcelas60']) }} parcelas no boleto</small></p></td>
+                                            <td class="ls-meio-neg"><div id="total60" value="{{ $total60 }}">R$ {{ toReais($refis['total60']) }}</div></td>
+                                            <td class="ls-meio-neg">
+                                                <select id="60" class="form-control nParcela">
+                                                    @foreach($refis['nParcelas60'] as $n)
+                                                        <option value="{{ $n }}">{{ $n }}x</option>
+                                                    @endforeach
+                                            </td>
+                                            <td id="parcelamento60" class="ls-meio-neg">R$ {{ toReais($refis['total60']/$refis['nParcelas60'][0]) }}</td>
+                                        @else
+                                        <td class="ls-meio-neg">Total c/ 60% de desconto<p class="text-left vermelho"><small>* não disponível devido ao valor mínimo de parcela</small></p></td>
+                                            <td class="ls-meio-neg"><div id="total60" value="0">-</div></td>
+                                            <td class="ls-meio-neg">-</td>
+                                            <td class="ls-meio-neg">-</td>
+                                        @endif
+                                    </tr>
+                                </tbody>
+                            </table>
+
+                            <h5 class="mt-3 mb-2"><i class="fas fa-level-up-alt rotate-90"></i>&nbsp;&nbsp;ANUIDADES COBRADAS</h5>
+                            <table class="table table-bordered bg-white mb-0">
+                                <tbody>
+                                    @foreach ($refis[anuidadesRefis] as $anuidade)
+                                        <tr>
+                                            <td class="ls-meio-neg">
+                                                {{ $anuidade }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+
+                        @else
+                        <p>Não é possível simular valores Refis para quitar anuidades em aberto devido a situação do representante comercial.</p>
+                        @endif
                         </div>
 
                     </div>
