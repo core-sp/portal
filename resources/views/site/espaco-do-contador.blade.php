d16857078fb5a74c4f65215ea87a4b9d5f329401
mikioi <mikioi@users.noreply.github.com>
2021-06-02 16:27:35 -0300

    Adicionando artes e textos.

--- resources/views/site/espaco-do-contador.blade.php
+++ resources/views/site/espaco-do-contador.blade.php
@@ -1,10 +1,10 @@
-@extends('site.layout.app', ['title' => 'Fiscalização'])
+@extends('site.layout.app', ['title' => 'Espaço do Contador'])
 
 @section('content')
 
 <section id="pagina-cabecalho">
   <div class="container-fluid text-center nopadding position-relative pagina-titulo-img">
-    <img src="{{ asset('img/banner-fiscalizacao.jpg') }}" />
+    <img src="{{ asset('img/banner-espaco-do-contador.png') }}" />
     <div class="row position-absolute pagina-titulo">
       <div class="container text-center">
         <h1 class="branco text-uppercase">
@@ -30,6 +30,35 @@
       </div>
     </div>
     <div class="linha-lg"></div>
+
+    <div class="conteudo-txt">
+      <p class="pb-0"><i>Novo ambiente virtual vai reunir documentos necessários e serviços essenciais para auxiliar os contadores que cuidam da rotina de representantes comerciais.</i></p></br>
+      <p class="pb-0">Além de registrar, orientar, normatizar e fiscalizar o exercício da profissão, o Conselho Regional dos Representantes Comerciais no Estado de São Paulo também busca garantir o bem-estar de toda a sociedade.</p></br>
+      <p class="pb-0">Pensando nisso, os Setores Jurídico, Financeiro, de Fiscalização e de Tecnologia de Informação uniram seus esforços e seus conhecimentos para criar um novo ambiente virtual em nosso Portal: <strong>O Espaço do Contador</strong>.</p></br>
+      <p class="pb-0">Nesta página, os profissionais de contabilidade vão encontrar as informações necessárias para solucionar questões de natureza burocrática, como o pagamento de taxas e regularização do registro, especialmente quando se trata de Pessoa Jurídica.</p></br>
+    </div>
+
+    <div class="linha-lg"></div>
+    <div class="mb-3">
+      <a href="https://www.core-sp.org.br/noticias/suspensao-por-inatividade" target="_blank"><img src="{{ asset('img/icone-suspensao.png') }}"></a>
+    </div>
+
+    <div class="mb-3">
+      <a href="https://www.core-sp.org.br/noticias/pagamento-de-anuidades" target="_blank"><img src="{{ asset('img/icone-pag-anuidades.png') }}"></a>
+    </div>
+
+    <div class="mb-3">
+      <a href="https://www.core-sp.org.br/noticias/refis-2021" target="_blank"><img src="{{ asset('img/icone-refis-2021.png') }}"></a>
+    </div>
+
+    <div class="col-12">
+      <div class="home-title">
+        <blockquote>
+          <i></i>
+          <h2 class="pr-3 ml-1">Notícias</h2>
+        </blockquote>
+      </div>
+    </div>
     <div class="row mt-2">
       @forelse($noticias as $noticia)
         @include('site.inc.noticia-grid')
