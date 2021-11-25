87abb7deef2f778bedb23381af6b151bad69ff3b
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2021-11-25 17:10:37 -0300

    Texto senha request

--- app/Http/Requests/PreRepresentanteRequest.php
+++ app/Http/Requests/PreRepresentanteRequest.php
@@ -41,7 +41,7 @@ class PreRepresentanteRequest extends FormRequest
             'min' => 'O campo possui menos caracteres que o mínimo necessário',
             'password.min' => 'A senha precisa ter, no mínimo, 8 caracteres.',
             'password.confirmed' => 'As senhas precisam ser idênticas entre si.',
-            'password.regex' => 'A senha deve conter um número, uma letra maiúscula e uma minúscula.',
+            'password.regex' => 'A senha deve ter, no mínimo, 8 caracteres contendo um número, uma letra maiúscula e uma minúscula.',
             'cpf_cnpj.regex' => 'Somente numeros devem ser inseridos.',
             'cpf_cnpj.unique' => 'Já existe esse CPF / CNPJ cadastrado no Pré-registro ou como Representante Comercial.',
         ];
