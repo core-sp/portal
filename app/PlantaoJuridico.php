a414ca55c1377ed325596f0b9d99a764c2867f52
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2022-01-27 10:36:49 -0300

    Model

--- app/PlantaoJuridico.php
+++ app/PlantaoJuridico.php
@@ -8,4 +8,9 @@ class PlantaoJuridico extends Model
 {
     protected $table = 'plantoes_juridicos';
     protected $guarded = [];
+
+    public function regional()
+    {
+    	return $this->belongsTo('App\Regional', 'idregional');
+    }
 }
