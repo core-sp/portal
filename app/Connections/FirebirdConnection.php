70cf68dd28fc4b07cf5e367ebab7032bee2d3cc5
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2022-12-05 10:58:14 -0300

    Em FirebirdConnection: chamando uma conexão opcional em caso de falha.

--- app/Connections/FirebirdConnection.php
+++ app/Connections/FirebirdConnection.php
@@ -18,7 +18,7 @@ class FirebirdConnection
                 $this->config()->password);
         } catch (PDOException $e) {
             \Log::error('[Erro: '.$e->getMessage().'], [Código: '.$e->getCode().'], [Arquivo: '.$e->getFile().'], [Linha: '.$e->getLine().']');
-            abort(500, 'Os servidores estão passando por manutenção. Por favor, tente dentro de alguns minutos.'/*'Estamos enfrentando problemas técnicos no momento. Por favor, tente dentro de alguns minutos.'*/);
+            $this->connect_opcional();
         }
 
         return $this->connection;
@@ -33,6 +33,36 @@ class FirebirdConnection
             'username' => env('GERENTI_USERNAME'),
             'password' => env('GERENTI_PASSWORD')
         ];
+    }
+
+    protected function config_opcional()
+    {
+        // Objeto com as informações de conexão
+        return (object) [
+            'host' => env('GERENTI_HOST_OPCIONAL'),
+            'dbname' => env('GERENTI_DATABASE'),
+            'username' => env('GERENTI_USERNAME'),
+            'password' => env('GERENTI_PASSWORD')
+        ];
+    }
+
+    protected function connect_opcional()
+    {   
+        try {
+            $message = 'Os servidores estão passando por manutenção. Por favor, tente dentro de alguns minutos.';
+			if(strlen(env('GERENTI_HOST_OPCIONAL')) == 0)
+                throw new \Exception('Não há ip opcional para realizar conexão com Gerenti', 500);
+            $this->connection = null;
+            $this->connection = new PDO('firebird:dbname='.$this->config_opcional()->host.':'.$this->config_opcional()->dbname.';charset=UTF8',
+                $this->config_opcional()->username,
+                $this->config_opcional()->password);
+        } catch (PDOException $e) {
+            \Log::error('[Erro: '.$e->getMessage().'], [Código: '.$e->getCode().'], [Arquivo: '.$e->getFile().'], [Linha: '.$e->getLine().']');
+            abort(500, $message);
+        } catch (\Exception $e) {
+            \Log::error('[Erro: '.$e->getMessage().'], [Código: '.$e->getCode().'], [Arquivo: '.$e->getFile().'], [Linha: '.$e->getLine().']');
+            abort(500, $message);
+        }
     }
 
     public function prepare($query)
