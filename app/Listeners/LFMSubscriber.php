4ef1007526da77d43c70ce87f3cdf5228b64c258
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2023-02-06 11:50:21 -0300

    LFMSubscriber: novos eventos e ajustes.

--- app/Listeners/LFMSubscriber.php
+++ app/Listeners/LFMSubscriber.php
@@ -10,12 +10,27 @@ use UniSharp\LaravelFilemanager\Events\ImageIsRenaming;
 use UniSharp\LaravelFilemanager\Events\ImageWasRenamed;
 use UniSharp\LaravelFilemanager\Events\FolderIsRenaming;
 use UniSharp\LaravelFilemanager\Events\FolderWasRenamed;
+use UniSharp\LaravelFilemanager\Events\ImageIsCropping;
+use UniSharp\LaravelFilemanager\Events\ImageWasCropped;
+use UniSharp\LaravelFilemanager\Events\ImageIsResizing;
+use UniSharp\LaravelFilemanager\Events\ImageWasResized;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Log;
 
 class LFMSubscriber
 {
+    private function getPath($path)
+    {
+        return str_replace(dirname($path, 4), '..', $path);
+    }
+
+    private function dispararLog($texto)
+    {
+        $ip = "[IP: " . request()->ip() . "] - ";
+        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') ' . $texto);
+    }
+
     public function subscribe($events)
     {
         $events->listen(
@@ -57,69 +72,111 @@ class LFMSubscriber
             FolderWasRenamed::class,
             'App\Listeners\LFMSubscriber@onFolderWasRenamed'
         );
-    }
 
-    public function handle($event)
-    {
-        $method = 'on'.class_basename($event);
-        if (method_exists($this, $method)) {
-            call_user_func([$this, $method], $event);
-        }
+        $events->listen(
+            ImageIsCropping::class,
+            'App\Listeners\LFMSubscriber@onImageIsCropping'
+        );
+
+        $events->listen(
+            ImageWasCropped::class,
+            'App\Listeners\LFMSubscriber@onImageWasCropped'
+        );
+
+        $events->listen(
+            ImageIsResizing::class,
+            'App\Listeners\LFMSubscriber@onImageIsResizing'
+        );
+
+        $events->listen(
+            ImageWasResized::class,
+            'App\Listeners\LFMSubscriber@onImageWasResized'
+        );
     }
 
     public function onImageIsDeleting(ImageIsDeleting $event)
     {
-        $path = $event->path();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') está excluindo o seguinte arquivo: ' . $path);
+        $path = $this->getPath($event->path());
+        $texto = 'está excluindo o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onImageWasDeleted(ImageWasDeleted $event)
     {
-        $path = $event->path();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') excluiu o seguinte arquivo: ' . $path);
+        $path = $this->getPath($event->path());
+        $texto = 'excluiu o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onImageIsUploading(ImageIsUploading $event)
     {
-        $path = $event->path();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') está anexando o seguinte arquivo: ' . $path);
+        $path = $this->getPath($event->path());
+        $texto = 'está anexando o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onImageWasUploaded(ImageWasUploaded $event)
     {
-        $path = $event->path();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') anexou o seguinte arquivo: ' . $path);
+        $path = $this->getPath($event->path());
+        $texto = 'anexou o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onImageIsRenaming(ImageIsRenaming $event)
     {
-        $path = $event->oldPath();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') está renomeando o seguinte arquivo: ' . $path);
+        $path = $this->getPath($event->oldPath());
+        $texto = 'está renomeando o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onImageWasRenamed(ImageWasRenamed $event)
     {
-        $path = $event->newPath();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') renomeou o arquivo antigo para: ' . $path);
+        $old_path = basename($event->oldPath());
+        $path = $this->getPath($event->newPath());
+        $texto = 'renomeou o arquivo "' . $old_path . '" para: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onFolderIsRenaming(FolderIsRenaming $event)
     {
-        $path = $event->oldPath();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') está renomeando a seguinte pasta: ' . $path);
+        $path = $this->getPath($event->oldPath());
+        $texto = 'está renomeando a seguinte pasta: ' . $path;
+        $this->dispararLog($texto);
     }
 
     public function onFolderWasRenamed(FolderWasRenamed $event)
     {
-        $path = $event->newPath();
-        $ip = "[IP: " . request()->ip() . "] - ";
-        Log::channel('interno')->info($ip . auth()->user()->nome.' (usuário '.auth()->user()->idusuario.') renomeou a pasta antiga para: ' . $path);
+        $old_path = basename($event->oldPath());
+        $path = $this->getPath($event->newPath());
+        $texto = 'renomeou o arquivo "' . $old_path . '" para: ' . $path;
+        $this->dispararLog($texto);
+    }
+
+    public function onImageIsCropping(ImageIsCropping $event)
+    {
+        $path = $this->getPath($event->path());
+        $texto = 'está cortando o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
+    }
+
+    public function onImageWasCropped(ImageWasCropped $event)
+    {
+        $path = $this->getPath($event->path());
+        $texto = 'cortou o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
+    }
+
+    public function onImageIsResizing(ImageIsResizing $event)
+    {
+        $path = $this->getPath($event->path());
+        $texto = 'está redimensionando o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
+    }
+
+    public function onImageWasResized(ImageWasResized $event)
+    {
+        $path = $this->getPath($event->path());
+        $texto = 'redimensionou o seguinte arquivo: ' . $path;
+        $this->dispararLog($texto);
     }
 }
