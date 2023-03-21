1b7cc16d3ce75a323f5f84690973a443148bc191
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2023-03-21 16:20:13 -0300

    MediadorService: melhoria na verificação do serviço.

--- app/Services/MediadorService.php
+++ app/Services/MediadorService.php
@@ -6,25 +6,14 @@ use App\Contracts\MediadorServiceInterface;
 
 class MediadorService implements MediadorServiceInterface {
 
-    private $service = [
-        'Suporte' => 'App\Contracts\SuporteServiceInterface',
-        'PlantaoJuridico' => 'App\Contracts\PlantaoJuridicoServiceInterface',
-        'Regional' => 'App\Contracts\RegionalServiceInterface',
-        'TermoConsentimento' => 'App\Contracts\TermoConsentimentoServiceInterface',
-        'Agendamento' => 'App\Contracts\AgendamentoServiceInterface',
-        'Licitacao' => 'App\Contracts\LicitacaoServiceInterface',
-        'Fiscalizacao' => 'App\Contracts\FiscalizacaoServiceInterface',
-        'Post' => 'App\Contracts\PostServiceInterface',
-        'Noticia' => 'App\Contracts\NoticiaServiceInterface',
-        'Cedula' => 'App\Contracts\CedulaServiceInterface',
-        'Representante' => 'App\Contracts\RepresentanteServiceInterface',
-    ];
-
+    // Nome do servico identico ao nome no Contrato na pasta App\Contracts.
     public function getService($nomeModel)
     {
-        if(isset($this->service[$nomeModel]))
-            return resolve($this->service[$nomeModel]);
-        \Log::error('[Erro: Serviço ' . $nomeModel . ' não encontrado no MediadorService.], [Código: 500], [Arquivo: App\Contracts\MediadorServiceInterface]');
-        abort(500, 'Serviço '.$nomeModel.' não encontrado no Sistema');
+        try{
+            return resolve('App\Contracts\\' . $nomeModel . 'ServiceInterface');
+        }catch(\Exception $e){
+            \Log::error('[Erro: Serviço ' . $nomeModel . ' não encontrado no MediadorService.], [Código: 500], [Arquivo: App\Contracts\MediadorServiceInterface]');
+            abort(500, 'Serviço '.$nomeModel.' não encontrado no Sistema');
+        }
     }
 }
