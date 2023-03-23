1b01f03d7cc4652aa55382430b725c0e42530435
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2023-03-23 14:51:01 -0300

    Blade app site: teste de uso da ferramenta da Microsoft.

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -34,6 +34,19 @@
           </script>
         @endif
         {!! mostraChatScript() !!}
+
+        <!-- ********************************************************************************************************************** -->
+        if(strpos(config('app.url'), 'homolog') !== false)
+        <!-- Teste de uso Clarity - Microsoft -->
+        <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "gd7y1ey5tx");
+        </script>
+        @endif
+        
     </head>
     <body>
 
