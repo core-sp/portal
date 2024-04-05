14b77ee1b1ca9c2340a76306cc7af71093c152ef
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2024-04-05 15:45:04 -0300

    tinymce.js: correção para focusout não remover img.

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -1,6 +1,6 @@
 // TinyMCE com File Manager
 var public_path = "{{ path('public') }}";  
-var css_galeria_popup = ['#tinyGaleria div.g-popup:not(:has([data-gallery]))', '#tinyGaleria :not(.g-popup)'];
+var css_galeria_popup = ['#tinyGaleria div.g-popup:not(:has([data-gallery]))', '#tinyGaleria :not(.g-popup, img)'];
 var editor_config = {
   license_key: 'gpl',
   path_absolute : "/",
