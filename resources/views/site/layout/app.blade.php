b587b4a8f3e0c2661f7e437f5ebc86597edf46ee
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2026-04-22 12:25:34 -0300

    Alteração de texto no banner: Atendimento.

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -82,7 +82,9 @@
             </div>
             <div class="col-lg-4 col-md-6 center-768 aai hide-768">
               <div class="d-block setembro">
-                <h3><a href="/servicos-atendimento-ao-rc" class="cinza-claro">Atendimento on-line</a></h3>
+                <h3><a href="/servicos-atendimento-ao-rc" class="cinza-claro">
+                  <span id="home-atende">Serviços de atendimento ao RC</span></a>
+                </h3>
                 <p class="light"><a href="/servicos-atendimento-ao-rc" class="cinza-claro"><small>Clique aqui e tenha acesso aos principais<br>serviços realizados pelo Setor de Atendimento do Core-SP.</small></a></p>
               </div>
             </div>
