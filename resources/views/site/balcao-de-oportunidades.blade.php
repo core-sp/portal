0169776fed51e61cf476f0518fdda54e7dfe9d81
CORCESP\randrade <desenvolvimento@core-sp.org.br>
2026-05-05 16:31:48 -0300

    Melhoria dos totais com largura menor de tela.

--- resources/views/site/balcao-de-oportunidades.blade.php
+++ resources/views/site/balcao-de-oportunidades.blade.php
@@ -50,13 +50,13 @@ use \App\BdoOportunidade;
 
         <!-- LG para baixo -->
         <div id="total_bdo_op" class="px-0 py-3 total_bdo hide-min-992">
-          <div class="bold ml-2 align-self-center branco">Oportunidades <br>disponíveis hoje</div>
-          <div class="bold total_op_emp branco total_bdo_texto numero_bdo_op">{{ $total_op }}</div>
+          <div class="col bold ml-2 align-self-center branco text-nowrap">Oportunidades <br>disponíveis hoje</div>
+          <div class="col bold branco total_bdo_texto numero_bdo_op">{{ $total_op }}</div>
         </div>
 
-        <div id="total_bdo_emp" class="px-0 py-3 float-right total_bdo hide-min-992">
-          <div class="bold total_op_emp azul-escuro total_bdo_texto numero_bdo_emp">{{ $total_emp }}</div>
-          <div class="bold mr-2 align-self-center azul-escuro">Empresas <br>cadastradas</div>
+        <div id="total_bdo_emp" class="px-0 py-3 total_bdo hide-min-992">
+          <div class="col bold azul-escuro total_bdo_texto numero_bdo_emp center-992">{{ $total_emp }}</div>
+          <div class="col bold mr-2 align-self-center azul-escuro right-max-991"><span class="p-3">Empresas</span> <br>cadastradas</div>
         </div>
 
         <!-- LG pra cima -->
